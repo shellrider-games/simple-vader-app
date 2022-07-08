e9c60e727852b729d69d121b0a733a5b849cb012
Georg Becker <georg.becker@shellrider.com>
2022-07-08 11:54:32 +0200

    Create endpoint for vader analyis

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+const vader = require('vader-sentiment');
+
+const app = express()
+const port = 8080
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + "/html/index.html");
+})
+
+app.post('/vader-analyse', (req, res) => {
+    const reply = {};
+    const text = req.body.text;
+    reply.text = text;
+    const sentiment = vader.SentimentIntensityAnalyzer.polarity_scores(text);
+    reply.sentiment = sentiment;
+
+    
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify(reply));
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
